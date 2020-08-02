9c450a05ca5a87d632777a6563ebd08407c42a80
harshitksrivastava <harsh0311@gmail.com>
2020-08-03 02:19:03 +0530

    completed all the templates, ready for deployment

--- jobs/models.py
+++ jobs/models.py
@@ -3,11 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Technology(models.Model):
-    technology_name = models.CharField(max_length=20)
+    technology_name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.technology_name
 
 
 class Project(models.Model):
     project_image= models.ImageField(upload_to='images/')
     project_title = models.CharField(max_length=20)
     project_summary= models.CharField(max_length=200)
-    project_tech = models.ManyToManyField(Technology)
+    project_tech = models.ManyToManyField(Technology,default=None)
+
+    def __str__(self):
+        return self.project_title
