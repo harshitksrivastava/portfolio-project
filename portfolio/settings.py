f079a7ef7b1bba6531a2a710dd38b4f51c1805a5
harshitksrivastava <harsh0311@gmail.com>
2020-08-04 01:28:30 +0530

    updated settings file

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'omc&eoo=q#*jmy4g-wdx8t0sr3sjd&r5^dscdf%k48r72j)#*@'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == True')
 
 ALLOWED_HOSTS = ['harshitksrivastava.herokuapp.com']
 
